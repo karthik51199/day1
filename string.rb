2a1b40768c16c624dbaf38d4ecd93e354d5e6267
karthik51199 <karthik51199@gmail.com>
2019-12-31 06:33:58 +0530

    adding string.rb file

---
+++ string.rb
@@ -0,0 +1,8 @@
+first_name = "syeera"
+last_name = "Reddy"
+middle_name = "Narshima"
+full_name1 = "#{first_name} #{middle_name} #{last_name}"
+full_name2 = first_name +' '+ middle_name +' '+ last_name
+#puts full_name1 , full_name2
+
+puts full_name1.include?("hasjd")
